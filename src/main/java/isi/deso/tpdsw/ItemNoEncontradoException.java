58fd5104176b5f70f8b8dbb7f8c87be6883eef65
Marcos Debona <167392105+MarcosDebona25@users.noreply.github.com>
2024-09-21 15:01:31 -0300

    Update ItemNoEncontradoException.java

--- src/main/java/isi/deso/tpdsw/ItemNoEncontradoException.java
+++ src/main/java/isi/deso/tpdsw/ItemNoEncontradoException.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package isi.deso.tpdsw;
 
-/**
- *
- * @author santi
- */
 public class ItemNoEncontradoException extends Exception{
     String mensaje;
     public ItemNoEncontradoException(String m){
         this.mensaje=m;
-        //System.out.println(mensaje);
     }
-    
 }
