a75a298b012873592adc2d8c1dc1fbec3fdad0ab
San-ti-P <paggisantino@gmail.com>
2024-10-30 20:24:34 -0300

    Se comienza con la siguiente etapa

---
+++ src/main/java/isi/deso/tpdsw/Views/ButtonRendererEditor.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package isi.deso.tpdsw.Views;
+
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableCellEditor;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonRendererEditor extends AbstractCellEditor implements TableCellRenderer, TableCellEditor, ActionListener {
+    private final JButton button;
+    private String label;
+
+    public ButtonRendererEditor() {
+        button = new JButton();
+        button.addActionListener(this);
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        button.setText((value == null) ? "Editar" : value.toString());
+        return button;
+    }
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        label = (value == null) ? "Editar" : value.toString();
+        button.setText(label);
+        return button;
+    }
+
+    @Override
+    public Object getCellEditorValue() {
+        return label;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(button, "Botón presionado en la fila " + label);
+        fireEditingStopped();
+    }
+}
